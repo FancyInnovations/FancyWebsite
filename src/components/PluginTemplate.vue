5f4cc1255ef34d114ca807e6b71edeaa6841b7de
OliverSchlueter <OliverSchlueter@users.noreply.github.com>
2024-01-20 14:52:52 +0100

    Update plugin main pages Update sitemap

--- src/components/PluginTemplate.vue
+++ src/components/PluginTemplate.vue
@@ -1,13 +1,6 @@
 <script setup lang="ts">
-  import {ref} from "vue";
-  import VueMarkdown from "vue-markdown-render";
   import Sidebar from "@/components/Sidebar.vue";
   const props = defineProps(["name"])
-
-  const docs = ref("Loading ...")
-  fetch("https://raw.githubusercontent.com/FancyMcPlugins/" + props.name + "/main/docs/docs.md")
-    .then(res => res.text())
-    .then(data => docs.value = data)
 </script>
 
 <template>
@@ -15,14 +8,12 @@
 
   <img v-if="name.toLowerCase() == 'fancynpcs'" class="title-img" src="/src/assets/img/fancynpcs_title.png" :alt="props.name">
   <img v-else class="title-img" src="/src/assets/img/fancyholograms_title.png" :alt="props.name">
-
-  <vue-markdown :source="docs" :options="{html: true}"/>
 </template>
 
 <style scoped>
 .title-img{
   width: 50%;
   transform: translateX(50%);
-  margin-bottom: 100px;
+  margin-bottom: 50px;
 }
 </style>
